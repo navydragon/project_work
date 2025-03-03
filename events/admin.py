88874e2d4f8a617ba818d1fe99915a36e6c09ca5
navydragon <navydragon@inbox.ru>
2025-03-03 11:08:02 +0300

    events_03_03

--- events/admin.py
+++ events/admin.py
@@ -4,13 +4,13 @@ from .models import Event, EventRequest
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ("date", "discipline", "tutor", "group", "type", "classroom")
-    list_filter = ("date", "discipline", "tutor", "group", "type")
-    search_fields = ("discipline", "tutor", "group", "type", "classroom")
+    list_display = ("date", "discipline", "tutor", "group", "type", "classroom","direction")
+    list_filter = ("date", "discipline", "tutor", "group", "type","direction")
+    search_fields = ("discipline", "tutor", "group", "type", "classroom","direction")
 
 
 @admin.register(EventRequest)
 class EventRequestAdmin(admin.ModelAdmin):
     list_display = ("event", "date", "full_name", "class_number", "school", "phone_number")
-    list_filter = ("date", "event", "school","direction")
-    search_fields = ("full_name", "class_number", "school", "phone_number","direction")
+    list_filter = ("date", "event", "school")
+    search_fields = ("full_name", "class_number", "school", "phone_number")
